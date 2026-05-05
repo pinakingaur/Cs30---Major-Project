d9a53811fa8309f5f5304540eccde43145480ec9
pinakingaur <pinakingaur11@gmail.com>
2026-05-05 15:24:38 -0600

    p5 play works

--- badminton/sketch.js
+++ badminton/sketch.js
@@ -6,11 +6,20 @@
 // - describe what you did to take this project "above and beyond"
 
 let sprite;
+let floor;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   sprite = new Sprite();
   sprite.width = 50;
   sprite.height = 50;
+  sprite.x = width/2;
+  sprite.y = height/2;
+  
+  floor = new Sprite();  
+  floor.y = 190;
+  floor.w = 238;
+  floor.h = 5;
+  floor.physics = STATIC;
 }
 
 function draw() {
