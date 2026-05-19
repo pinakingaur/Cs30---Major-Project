e14d6d739eaec06beb4ab3ada293a841414696ed
pinakingaur <pinakingaur11@gmail.com>
2026-05-18 18:15:09 -0600

    removed and added a court

--- badminton/sketch.js
+++ badminton/sketch.js
@@ -5,46 +5,22 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const LINE_DISTANCE = 100;
+let floor;
+let net;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  drawCourt();
+  new Canvas(1000, 600);
+  displayMode(CENTER);
+  world.gravity.y = 12;
+
+  //creates the floor and net
+  floor = new Sprite(500, 570, 900, 40, "static");
+  floor.color = color(0);
+
+  net = new Sprite(500, 420, 10, 220, "static");
+  net.color = color(255);
 }
 
 function draw() {
-  background(50);
-}
-
-function drawCourt() {
-  // the most bottom line of the court
-  let bottomLine = new Sprite();  
-  bottomLine.y = height - LINE_DISTANCE;
-  bottomLine.x = width/2;
-  bottomLine.h = 5;
-  bottomLine.w = width;
-  bottomLine.color = "white";
-
-  // the second bottom line of the court
-  let bottomLine2 = new Sprite();  
-  bottomLine2.y = height - LINE_DISTANCE*2;
-  bottomLine2.x = width/2;
-  bottomLine2.h = 5;
-  bottomLine2.w = width;
-  bottomLine2.color = "white";
-
-  // the highest line of the court
-  let topLine = new Sprite();  
-  topLine.y = LINE_DISTANCE;
-  topLine.x = width/2;
-  topLine.h = 5;
-  topLine.w = width;
-  topLine.color = "white";
-
-  // the second highest line of the court
-  let topLine2 = new Sprite();  
-  topLine2.y = LINE_DISTANCE*2;
-  topLine2.x = width/2;
-  topLine2.h = 5;
-  topLine2.w = width;
-  topLine2.color = "white";
+  background(35, 140, 80);
 }
