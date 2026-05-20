ceb8e84ca94e8e7d0f7019c49e560d48b37bf513
pinakingaur <pinakingaur11@gmail.com>
2026-05-19 20:56:31 -0600

    added birdie physics

--- badminton/sketch.js
+++ badminton/sketch.js
@@ -17,6 +17,9 @@ function setup() {
   new Canvas(1000, 600);
   displayMode(CENTER);
   starting_scene();
+  
+  // birdie physics
+  birdie.vel.x = random([-5, 5]);
 }
 
 function draw() {
@@ -24,6 +27,8 @@ function draw() {
 
   drawCourt();
   movement();
+  drawBirdieGraphic();
+  hitBirdie();
 }
 
 function drawCourt() {
@@ -31,45 +36,45 @@ function drawCourt() {
   const LINEY1 = 180;
   const LINEY2 = 540;
 
-  line(LINEX, LINEY1, LINEX, LINEY2);
-  line(LINEX * 5, LINEY1, LINEX * 5, LINEY2);
-  line(LINEX * 15, LINEY1, LINEX * 15, LINEY2);
-  line(LINEX * 20 - LINEX, LINEY1, LINEX * 20 - LINEX, LINEY2); 
+  line(LINEX, LINEY1, LINEX, LINEY2);   // the first line
+  line(LINEX * 5, LINEY1, LINEX * 5, LINEY2);   // the second line
+  line(LINEX * 15, LINEY1, LINEX * 15, LINEY2);   // the third line
+  line(LINEX * 20 - LINEX, LINEY1, LINEX * 20 - LINEX, LINEY2);   // the fourth line
 }
 
 function movement() {
-  const Running = 5;
-  const Jumping = 11;
+  const RUNNING = 5;
+  const JUMPING = 11;
   
   // player 1 
   player1.vel.x = 0;
-  if (kb.pressing("a")) {
-    player1.vel.x = -Running;
+  if (kb.pressing("a")) {     // to go left
+    player1.vel.x = -RUNNING;
   }
-  if (kb.pressing("d")) {
-    player1.vel.x = Running;
+  if (kb.pressing("d")) {     // to go right
+    player1.vel.x = RUNNING;
   }
-  if (kb.presses("w") && player1.colliding(floor)) {
-    player1.vel.y = -Jumping;
+  if (kb.presses("w") && player1.colliding(floor)) {    // to jump
+    player1.vel.y = -JUMPING;
   }
   
   // player 2
   player2.vel.x = 0;
-  if (keyIsDown(LEFT_ARROW)) {
-    player2.vel.x = -Running;
+  if (keyIsDown(LEFT_ARROW)) {      // to go left
+    player2.vel.x = -RUNNING;
   }
-  if (keyIsDown(RIGHT_ARROW)) {
-    player2.vel.x = Running;
+  if (keyIsDown(RIGHT_ARROW)) {     // to go right
+    player2.vel.x = RUNNING;
   }
-  if (keyIsDown(UP_ARROW) && player2.colliding(floor)) {
-    player2.vel.y = -Jumping;
+  if (keyIsDown(UP_ARROW) && player2.colliding(floor)) {      // to jump
+    player2.vel.y = -JUMPING;
   }
 }
 
 function starting_scene() {
   world.gravity.y = 12;
 
-  //creates the floor, net, players and birdie
+  // creates the floor, net, players and birdie
   floor = new Sprite(500, 570, 900, 40, "static");
   floor.color = color(0);
 
@@ -81,4 +86,52 @@ function starting_scene() {
 
   birdie = new Sprite(500, 200, 18);
   birdie.color = "white";
+}
+
+function drawBirdieGraphic() {
+  push();
+  translate(birdie.x, birdie.y);
+  rotate(frameCount * 0.05);
+  fill(255);
+  ellipse(0, 0, 18);
+  triangle(-10, 5, 10, 5, 0, 28);
+  pop();
+}
+
+function hitBirdie() {
+  const BIRDIE_VELOCITY = 9;
+
+ // player 1 hit
+ if (kb.pressing("f")) {
+  let d = dist(
+     player1.x,
+     player1.y - 40,
+     birdie.x,
+     birdie.y
+   );
+   if (d < 90) {
+     birdie.vel.x = BIRDIE_VELOCITY;
+     birdie.vel.y = -BIRDIE_VELOCITY;
+   }
+ }
+
+ // player 2 hit
+  if (keyIsDown(191)) {   // It means the "/" key
+    let d = dist(
+     player2.x,
+     player2.y - 40,
+     birdie.x,
+     birdie.y
+   );
+   if (d < 90) {
+    birdie.vel.x = -BIRDIE_VELOCITY;
+    birdie.vel.y = -BIRDIE_VELOCITY;
+   }
+ }
+
+ // completely stops birdie as soon as it hits the floor
+ if (birdie.colliding(floor)) {
+  birdie.vel.x = 0;
+  birdie.vel.y = 0;
+}
 }
